68eb41440ef4a027f0440f8c427e0e91d6629a8e
Tendai <tnyandoro@gmail.com>
2025-01-06 11:31:56 +0200

    Update the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
+    if params[:role] && !User.roles.key?(params[:role])
+      render json: { error: 'Invalid role' }, status: :unprocessable_entity
+      return
+    end
+
     @pagy, @users = pagy(@organization.users.filter_by_role(params[:role]))
     render json: {
       users: UserSerializer.new(@users).serializable_hash,
@@ -23,6 +28,12 @@ class UsersController < ApplicationController
   def create
     @user = @organization.users.new(user_params)
 
+    # Prevent non-admins from creating admins
+    if user_params[:role] == 'admin' && !current_user.role_admin?
+      render json: { error: 'You are not authorized to create an admin user' }, status: :forbidden
+      return
+    end
+
     if @user.save
       render json: UserSerializer.new(@user).serializable_hash, status: :created, location: @user
     else
@@ -30,8 +41,13 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
   def update
+    # Prevent non-admins from updating users to admin
+    if user_params[:role] == 'admin' && !current_user.role_admin?
+      render json: { error: 'You are not authorized to update this user to admin' }, status: :forbidden
+      return
+    end
+
     if @user.update(user_params)
       render json: UserSerializer.new(@user).serializable_hash
     else
@@ -54,7 +70,8 @@ class UsersController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions
   def set_user
-    @user = @organization.users.find(params[:id])
+    @user = @organization.users.find_by(id: params[:id])
+    render json: { error: 'User not found' }, status: :not_found unless @user
   end
 
   # Only allow a list of trusted parameters through
@@ -63,8 +80,9 @@ class UsersController < ApplicationController
   end
 
   # Ensure only admins can perform certain actions
+
   def authorize_admin
-    unless current_user&.admin?
+    unless current_user&.role_admin? || (current_user&.role_teamlead? && @user&.role_agent? || @user&.role_viewer?)
       render json: { error: 'You are not authorized to perform this action' }, status: :forbidden
     end
   end
