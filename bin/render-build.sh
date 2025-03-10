ba8fd1c006b6e876f2b4bc23969d1e0c380eee3b
Tendai <tnyandoro@gmail.com>
2025-03-10 14:21:42 +0200

    update postgresql

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,15 +4,5 @@
 # Install dependencies
 bundle install
 
-# Precompile assets
-bundle exec rails assets:precompile
-
 # Run database migrations (optional, if preDeployCommand is not available)
-# bundle exec rails db:migrate
-bundle exec rails assets:clean
-
-# If you're using a Free instance type, you need to
-# perform database migrations in the build command.
-# Uncomment the following line:
-
-# bundle exec rails db:migrate
+bundle exec rails db:migrate
