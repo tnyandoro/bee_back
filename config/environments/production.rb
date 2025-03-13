c69fa02000a945f716cee500185804d6b512f7aa
Vibha2286 <vibha.pawar@rediffmail.com>
2025-03-13 13:49:34 +0200

    Updated seed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,7 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
+  config.active_record.lock_timeout = 10000
 
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
