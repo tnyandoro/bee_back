5833205eb505eda5d2e4c7210ab50167510a59b6
Vibha2286 <vibha.pawar@rediffmail.com>
2025-03-17 11:20:52 +0200

    Updated user controller11

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -117,19 +117,20 @@ module Api
       # end
 
       def set_organization_from_subdomain
-        # Prioritize params[:subdomain] over request.subdomain
-        subdomain = params[:subdomain].presence || request.subdomain.presence || 'default'
+        # Prioritize `organization_subdomain` over `subdomain` and request subdomain
+        subdomain = params[:organization_subdomain].presence || params[:subdomain].presence || request.subdomain.presence || 'default'
         
         Rails.logger.info "Subdomain detected from params: '#{params[:subdomain]}'"
+        Rails.logger.info "Organization subdomain from params: '#{params[:organization_subdomain]}'"
         Rails.logger.info "Subdomain detected from request: '#{request.subdomain}'"
         Rails.logger.info "Final subdomain used: '#{subdomain}'"
       
         @organization = Organization.find_by!(subdomain: subdomain)
       rescue ActiveRecord::RecordNotFound
         Rails.logger.error "Organization not found for subdomain: #{subdomain}"
-        render json: { error: 'Organization not found for this subdomain' }, status: :not_found
+        render json: { error: 'Organization not found for this subdomain for user' }, status: :not_found
       end
-      
+
 
       def verify_user_organization
         unless current_user&.organization_id == @organization.id
