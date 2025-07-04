bbb35e4131f3376a39b352e02b657ec6d5554b5d
Tendai <tnyandoro@gmail.com>
2025-07-05 00:09:56 +0200

    update the seetings controller

--- app/controllers/api/v1/settings_controller.rb
+++ app/controllers/api/v1/settings_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class SettingsController < ApplicationController
-      before_action :authenticate_api_user!
+      before_action :authenticate_user!
       before_action :set_organization
 
       # GET /api/v1/organizations/:subdomain/settings
