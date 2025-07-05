05d3d4fe00f7ed653d431c8e02e140aa78f17058
Tendai <tnyandoro@gmail.com>
2025-07-05 11:24:08 +0200

    Add cloudinary and a picture aassociaation to the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :auth_token
   has_one_attached :avatar
+  has_one_attached :profile_picture
 
   attr_accessor :skip_auth_token
 
