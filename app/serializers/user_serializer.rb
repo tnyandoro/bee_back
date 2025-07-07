39fa00cf014f2f23496ce47325ce752bc8abaa5f
Tendai <tnyandoro@gmail.com>
2025-07-07 10:09:16 +0200

    Update the serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,8 +2,12 @@
 class UserSerializer
     include JSONAPI::Serializer
   
-    attributes :id, :name, :email, :role, :department, :position, :created_at, :updated_at
+    attributes :id, :name, :email, :username, :phone_number, :department, :position, :role, :team_id, :profile_picture_url, :created_at, :updated_at
   
     belongs_to :organization, serializer: OrganizationSerializer
     belongs_to :team, serializer: TeamSerializer
-  end
+
+    def profile_picture_url
+      object.profile_picture.attached? ? Rails.application.routes.url_helpers.url_for(object.profile_picture) : nil
+    end
+end
