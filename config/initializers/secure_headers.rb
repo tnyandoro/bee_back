79c0d3eda7ea52e26cf36190e12ecb80f287394f
Tendai <tnyandoro@gmail.com>
2025-08-15 17:50:50 +0200

    Add headers

---
+++ config/initializers/secure_headers.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+Rails.application.config.action_dispatch.default_headers.merge!({
+  # Clickjacking protection
+  "X-Frame-Options" => "DENY",
+
+  # XSS protection for legacy browsers
+  "X-XSS-Protection" => "1; mode=block",
+
+  # Prevent MIME type sniffing
+  "X-Content-Type-Options" => "nosniff",
+
+  # Control referrer information
+  "Referrer-Policy" => "strict-origin-when-cross-origin",
+
+  # Force HTTPS (HSTS)
+  "Strict-Transport-Security" => "max-age=31536000; includeSubDomains"
+})
