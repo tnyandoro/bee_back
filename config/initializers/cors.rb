b931f6fd26ff671f7c1661cd1056cb24e5f21382
Vibha2286 <vibha.pawar@rediffmail.com>
2025-08-15 13:49:50 +0200

    updated syntax

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,11 +8,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         'http://localhost:3001',
         'http://lvh.me:3001',
         'https://d10tmedpan81b6.cloudfront.net',
-        'https://gsolve360.greensoftsolutions.net/', 
-        'https://www.gsolve360.greensoftsolutions.net/', 
+        'https://gsolve360.greensoftsolutions.net', 
+        'https://www.gsolve360.greensoftsolutions.net', 
         'https://www.greensoftsolutions.net'
         /\.lvh\.me(:\d+)?$/,
-        /\.localhost(:\d+)?$/
+        /\.localhost(:\d+)?$/,
       )
 
       resource '*',
