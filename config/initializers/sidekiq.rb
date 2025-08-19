4f46b7da6b5b28bf57462d04271a44bf3f63a657
Vibha <vibha.pawar@rediffmail.com>
2025-08-19 10:08:02 +0200

    Updated redis(Vibha)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379')
+redis_url = ENV.fetch('REDIS_URL', 'redis://default:zKnV0z9cfr4J6GVk4WQ5@10.0.13.21:6379')
 
 Sidekiq.configure_server do |config|
   config.redis = {
